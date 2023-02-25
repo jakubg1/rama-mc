836d9f7d5d2f45f3277cd28a14dde096d355f827
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2023-02-25 13:23:40 -0800

    Players deal more damage during day
    Closes #1

--- src/main/java/skycat/ramamc/mixin/PlayerEntityMixin.java
+++ src/main/java/skycat/ramamc/mixin/PlayerEntityMixin.java
@@ -4,6 +4,7 @@ import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import skycat.ramamc.RamaMc;
 
@@ -12,5 +13,13 @@ public abstract class PlayerEntityMixin {
     @Inject(method = "canConsume", at = @At("RETURN"), cancellable = true)
     public void canConsume(boolean ignoreHunger, CallbackInfoReturnable<Boolean> cir) {
         cir.setReturnValue(!RamaMc.isDay()); // Stop from eating if it is day, allow if it is night
+    }
+
+    @ModifyArg(method = "attack", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;damage(Lnet/minecraft/entity/damage/DamageSource;F)Z"), index = 1)
+    public float modifyAttackAmount(float amount) {
+        if (RamaMc.isDay()) {
+            return amount * 1.1F;
+        }
+        return amount;
     }
 }
